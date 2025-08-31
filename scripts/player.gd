6cf7e1fe3a4dfa8d5e7e8cdee960a8334ae8ddcf
JHONATHAN LOBATO <jhonathan.lobato@proton.me>
2025-08-31 15:37:33 -0300

    Ultimas alterações do dia, e adição da plataforma movel

---
+++ scripts/player.gd
@@ -0,0 +1,35 @@
+extends CharacterBody2D
+
+const SPEED = 200.0
+const JUMP_VELOCITY = -400.0 # JUMP FORCE
+
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var is_jumping := false
+
+@onready var animation := $anim as AnimatedSprite2D
+
+func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	
+	# if para saber se está pulando
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		is_jumping = true
+	elif is_on_floor():
+		is_jumping = false
+		
+	var direction = Input.get_axis("ui_left", "ui_right")
+	# condicional para saber se o player está andando
+	if direction != 0:
+		velocity.x = direction * SPEED
+		animation.scale.x = direction
+		if not is_jumping:
+			animation.play("run")
+	elif is_jumping:
+		animation.play("jump")
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		animation.play("idle")
+		
+	move_and_slide()
