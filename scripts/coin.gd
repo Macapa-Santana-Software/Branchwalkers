8f5f5435769dd5b7ed5be72c8cf7582eecf55726
JHONATHAN LOBATO <jhonathan.lobato@proton.me>
2025-09-04 21:57:19 -0300

    Enviando o inimigo e itens, e alterações no cenario

---
+++ scripts/coin.gd
@@ -0,0 +1,13 @@
+extends Area2D
+
+func _ready() -> void:
+	pass
+	
+func _process(delta: float) -> void:
+	pass
+
+func _on_body_entered(body: Node2D) -> void:
+	$anim.play("collect")
+
+func _on_anim_animation_finished() -> void:
+	queue_free()
